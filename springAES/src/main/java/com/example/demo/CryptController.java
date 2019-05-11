1b213db3b952d4e7340a8307f3d764da8cc0f7fb
Viktor Tolstoi <vic_t@ukr.net>
2019-05-11 14:09:38 +0300

    Added MyErrorController

--- springAES/src/main/java/com/example/demo/CryptController.java
+++ springAES/src/main/java/com/example/demo/CryptController.java
@@ -12,12 +12,12 @@ public class CryptController {
     Crypter crypt = new Crypter();
 
     private static final String template = "Hello, %s!" +
-        "Query 1 get default value 'Example for Concord' or value from parameter encrypt. " +
+        "Query 1 get default value 'Example for Concord' or value from parameter encrypt." +
         "Example: http://localhost:8080/encrypt?encrypt=text_for_coding. " +
         "Query 2 has not a default value, it gets the coding string from a common object Crypter" +
         "or value from parameter decrypt. " +
         "Example: http://localhost:8080/decrypt?decrypt=17a90551c50d406a2dc55fd7b33b5396ed21cbf403685a53a5e2f0568437fe2c. " +
-        "When we use Query 2 before Query 1 we get an error because the coded string is not exist";
+        "When we use Query 2 before Query 1 we get an error because the coded string is not exist.";
 
     private final AtomicLong counter = new AtomicLong();
 
@@ -48,9 +48,4 @@ public class CryptController {
         return crypt;
     }
 
-    /*@RequestMapping("/error")
-    public ErrorMessage errorMessage() {
-
-        return new ErrorMessage("The application has some error.");
-    }*/
 }
