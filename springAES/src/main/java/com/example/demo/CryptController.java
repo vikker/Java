a8290e9a789edbda887987e1c5e56d70d3b403ba
Viktor Tolstoi <vic_t@ukr.net>
2019-05-11 07:11:23 +0300

    Update and rename GreetingController.java to CryptController.java

--- springAES/src/main/java/com/example/demo/CryptController.java
+++ springAES/src/main/java/com/example/demo/CryptController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class GreetingController {
+public class CryptgController {
 
     Crypter crypt = new Crypter();
 
