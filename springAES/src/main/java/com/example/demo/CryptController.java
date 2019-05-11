633f4981ead7560fa426a1a1cb3179aa147a72ff
Viktor Tolstoi <vic_t@ukr.net>
2019-05-11 13:46:54 +0300

    Added mapping "/", renamed into CryptController

--- springAES/src/main/java/com/example/demo/CryptController.java
+++ springAES/src/main/java/com/example/demo/CryptController.java
@@ -4,11 +4,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class CryptgController {
+public class CryptController {
 
     Crypter crypt = new Crypter();
+
+    private static final String template = "Hello, %s!" +
+        "Query 1 get default value 'Example for Concord' or value from parameter encrypt. " +
+        "Example: http://localhost:8080/encrypt?encrypt=text_for_coding. " +
+        "Query 2 has not a default value, it gets the coding string from a common object Crypter" +
+        "or value from parameter decrypt. " +
+        "Example: http://localhost:8080/decrypt?decrypt=17a90551c50d406a2dc55fd7b33b5396ed21cbf403685a53a5e2f0568437fe2c. " +
+        "When we use Query 2 before Query 1 we get an error because the coded string is not exist";
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/")
+    public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="Concord") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
 
     @RequestMapping("/encrypt")
     public Crypter encrypt(@RequestParam(value="encrypt", defaultValue="Example for Concord") String encrypt) {
@@ -30,4 +47,10 @@ public class CryptgController {
 
         return crypt;
     }
+
+    /*@RequestMapping("/error")
+    public ErrorMessage errorMessage() {
+
+        return new ErrorMessage("The application has some error.");
+    }*/
 }
---
+++ springAES/src/main/java/com/example/demo/ErrorMessage.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class ErrorMessage {
+
+    public String message;
+
+    public ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
